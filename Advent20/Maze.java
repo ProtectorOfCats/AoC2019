b986c97cc13bec439bb6ab8ef0d48d07ddaa0c85
ProtectorOfCats <78569811+ProtectorOfCats@users.noreply.github.com>
2021-02-04 16:39:22 -0800

    Create Maze.java
    Maze holds what # "pocket dimension" "you" are currently on, and what tiles are relevant to "you", as well as all the portals you've gone through to get here.

---
+++ Advent20/Maze.java
@@ -0,0 +1,67 @@
+import java.util.*;
+class Maze{
+	private int layer = -1;
+	private ArrayList<Tile> tiles;
+	private Tile entrance = null, exit = null;
+	private ArrayList<Integer> path;
+	private String tab;
+	public Maze(int l, ArrayList<Integer> p, String T){
+		tab = T;
+		path = p;
+		layer = l;
+		if(l == 0){
+			tiles = Layout.layer_zero();
+		}else{
+			tiles = Layout.not_layer_zero();
+		}
+		for(Tile t : tiles){
+			if(t.value().equals("AA")) entrance = t;
+			if(t.value().equals("ZZ")) exit = t;
+			if(entrance != null && exit != null) break;
+		}
+	}
+
+	public HashMap<Tile[],Maze> traverse(Tile start){
+		if(layer == 0 || layer == 1){
+			for(Tile t : tiles){
+				if(t.y() == start.y() && t.x() == start.x()){
+					if(t != start){
+						start = t;
+					}
+					break;
+				}
+			}
+		}
+		HashMap<Tile[],Maze> retval = new HashMap<>();
+		ArrayList<Integer> passOn = new ArrayList<>(path);
+		passOn.add((start.ID() * 1000) + layer);
+		ArrayList<Tile> keys = start.keys();
+		for(int i = 0; i < keys.size(); i++){
+			int newLayer = layer;
+			if(keys.get(i).outerCircle()){
+				newLayer--;
+			}else if(keys.get(i).innerCircle()){
+				newLayer++;
+			}
+			if(keys.get(i) == exit || keys.get(i) == entrance){
+				retval.put(new Tile[]{ null,keys.get(i) }, new Maze(newLayer,passOn,""));
+			}else if(newLayer >= 0){
+				Maze subMaze = null;
+				boolean equals = true;
+				for(Maze m : retval.values()){
+					if(m.layer == newLayer){
+						subMaze = m;
+						break;
+					}
+				}
+				if(subMaze == null) subMaze = new Maze(newLayer,passOn,tab + "  ");
+				retval.put(new Tile[]{ null,keys.get(i) },subMaze);
+			}
+		}
+		return retval;
+	}
+
+	public int layer(){ return layer; }
+	public ArrayList<Integer> getPath(){ return path; }
+	public ArrayList<Tile> tiles(){ return tiles; }
+}
